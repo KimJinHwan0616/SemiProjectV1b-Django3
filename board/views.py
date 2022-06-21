cf3018fd96d7bdc2b4d5e9a08aafba2b5603fe34
KimJinHwan <wlsghks0616@naver.com>
2022-06-21 12:37:43 +0900

    Django3 app urls.py, views.py 수정 - home, member, board

--- board/views.py
+++ board/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def list(request):
+    return render(request, 'board/list.html')
+
+
+def view(request):
+    return render(request, 'home/view.html')
+
+
+def write(request):
+    return render(request, 'member/write.html')
--- member/views.py
+++ member/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def list(request):
+    return render(request, 'member/join.html')
+
+
+def view(request):
+    return render(request, 'member/login.html')
+
+
+def write(request):
+    return render(request, 'member/myinfo.html')
