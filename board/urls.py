a38e57470221a042d0911d8a4baf1c87ba0c0bb6
KimJinHwan <wlsghks0616@naver.com>
2022-06-27 17:34:49 +0900

    Django3 board 리스트/본문글 출력, 본문글 수정/삭제하기, 본문글 조회수 증가, 리스트 no-cache, 본문글 줄바꿈 적용, 리스트 글번호 수정

--- board/urls.py
+++ board/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('list/', views.list, name='list'),
     path('view/', views.view, name='view'),
     path('write/', views.write, name='write'),
+    path('modify/', views.modify, name='modify'),
+    path('remove/', views.remove, name='remove'),
 ]
--- board/views.py
+++ board/views.py
@@ -1,13 +1,105 @@
-from django.shortcuts import render
+from django.db.models import F
+from django.shortcuts import render, redirect
+from django.views.decorators.cache import cache_control
 
-# Create your views here.
+from board.models import Board
+
+# 게시판 목록보기 처리
+from member.models import Member
+
+@cache_control(no_cache=True, no_store=True, must_revalidate=True)
 def list(request):
+    # select 'id', 'title', 'userid', 'regdate', 'views'
+    # from borad order by id desc
+
+    # bdlist = Board.objects.values(
+    #         'id', 'title', 'userid', 'regdate', 'views')\
+    #         .order_by('-id')
+
+    # Board와 Member 테이블은 userid <-> id 컬럼을 기준으로 inner join을 실행
+    bdlist = Board.objects.select_related('board')
+
+    # join된 member 테이블의 userid 확인
+    # bdlist.get(0).member.userid
+
+    context = {'bds': bdlist}
     return render(request, 'board/list.html')
 
-
+# 게시판 본문보기 처리
+@cache_control(no_cache=True, no_store=True, must_revalidate=True)
 def view(request):
-    return render(request, 'home/view.html')
+    if request.get == 'GET':
 
+    form = request.GET.dict()
+    # print(form['bno'])
+
+    # 본문글에 대한 조회수 증가
+    # update board set views = views + 1
+    # where id = ???
+
+    # 이해
+    # b = Board.objects.get(id=form['bno'])
+    # b.views = b.views + 1
+    # b.save()
+
+    # 실전
+    Board.objects.filter(id=form['bno'])\
+        .update(views=F('views') + 1)
+
+
+    # 본문글 조회
+    # select * from board inner join member
+    # where id = ???
+    bd = Board.objects.select_related('member')\
+        .get(id=form['bno'])
+
+    elif request.method == 'POST':
+        pass
+
+    return render(request, 'board/view.html')
 
 def write(request):
-    return render(request, 'member/write.html')
+
+    return render(request, 'board/write.html')
+
+# 본문글 삭제하기
+# /remove?bno=***
+def remove(request):
+    if request.method == 'GET':
+        form = request.GET.dict()
+
+        # delete from board where bno = ??
+        Board.objects.filter(id=form['bno']).delete()
+
+    return redirect('/list')
+
+
+def modify(request):
+    if request.method == 'GET':
+        form = request.GET.dict()
+
+        # select * from board where bno = ???
+        Board.objects.get(id=form['bno'])
+
+
+    elif request.method == 'POST':
+        form = request.POST.dict()
+
+        # update board set title = ???, contents = ???
+        # where bno = ???
+
+        # 이해
+        # b = Board.objects.get(id=form['bno'])
+        # b.title = form['title']
+        # b.contents = form['contents']
+        # b.save()
+
+        # 실전
+        Board.objects.filter(id=form['bno'])\
+              .update(title=form['title'], contents=form['contents'])
+
+        # 본문글 수정완료시 view 페이지로 이동
+        redirect('/view?bno=' + form['bno'])
+
+    context = {'bd': bd}
+    return render(request, 'board/modify.html', context)
